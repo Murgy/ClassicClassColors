c019344a5614fcf66cd5fa7dba725a907f6d1616
Tyler <murgymail@gmail.com>
2021-05-20 11:18:03 -0500

    TBC
    Re Release for TBC

--- ClassicClassColors.lua
+++ ClassicClassColors.lua
@@ -1,13 +1,7 @@
-local function eventHandler(self,event,...)
-	RAID_CLASS_COLORS['SHAMAN']["colorStr"] = "ff0070de"
-	RAID_CLASS_COLORS['SHAMAN']["r"] = 0.0
-	RAID_CLASS_COLORS['SHAMAN']["g"] = 0.44
-	RAID_CLASS_COLORS['SHAMAN']["b"] = 0.87
-	SetCVar("chatClassColorOverride", "0")
+local function OnEvent(self, event)
+	SetCVar("chatClassColorOverride ", 0)
 end
 
-local characterLogin = CreateFrame("Frame")
-
-characterLogin:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-characterLogin:SetScript("OnEvent", eventHandler)
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", OnEvent)
